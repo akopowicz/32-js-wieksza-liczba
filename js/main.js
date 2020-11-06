2588a507046dd2c0403997970c530c165c7e84df
akopowicz <anna.kopowicz@gmail.com>
2020-11-06 12:24:05 +0100

    zadanie wykonane

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let num1 = 2;
+let num2 = 5;
+
+function number() {
+    if (num1 > num2) {
+        result = num1;
+    } else {
+        result = num2;
+    }
+return result;
+}
+
+console.log(number());
+
